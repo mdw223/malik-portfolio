2f9ef791b58ab81274752526689be11bf7309192
mdwensma <mdwensma@ncsu.edu>
2025-12-20 03:26:05 -0500

    fixed header logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -91,7 +91,7 @@ export function Header() {
           <Link href="/" className="flex items-center gap-2" data-testid="link-home">
             <div className="w-8 h-8 rounded-lg bg-primary flex items-center justify-center">
               {/* <span className="text-primary-foreground font-bold text-sm">MW</span> */}
-              <img src="/favicon.png" alt="" />
+              <img src="./favicon.png" alt="" />
             </div>
             <span className="font-semibold text-lg hidden sm:block">{personalInfo.name}</span>
           </Link>
