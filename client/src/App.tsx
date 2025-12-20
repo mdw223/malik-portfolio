a354dc9339bfb80f6113b9ca0b0afceaa8f208f6
mdwensma <mdwensma@ncsu.edu>
2025-12-20 01:46:05 -0500

    fixed dev routing, and router page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,25 +7,37 @@ import { ThemeProvider } from "@/lib/theme";
 import Home from "@/pages/Home";
 import Blog from "@/pages/Blog";
 import NotFound from "@/pages/not-found";
+import { useEffect } from "react";
 
-function Router() {
+function AppRouter() {
+  const base = import.meta.env.PROD ? '/malik-portfolio' : '';
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/blog/:slug" component={Blog} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router base={base}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/blog/:slug" component={Blog} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
 function App() {
+  // Handle 404 redirects
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get('redirect');
+    if (redirect) {
+      window.history.replaceState({}, '', '/malik-portfolio' + redirect);
+    }
+  }, []);
   return (
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <AppRouter />
         </TooltipProvider>
       </QueryClientProvider>
     </ThemeProvider>
