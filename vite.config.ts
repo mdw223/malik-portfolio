3acf020fed290952bc0b9ff1e3ecc882856d68be
mdwensma <mdwensma@ncsu.edu>
2025-12-20 00:55:17 -0500

    fixing 404 after passed deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/malik-portfolio/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
